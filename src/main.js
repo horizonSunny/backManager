9934cd30b5142fef6e548ef851603ea7fb5ae250
sunyu1 <sunyu1@green-valley.com>
2019-10-08 15:52:42 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
