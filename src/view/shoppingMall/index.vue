baa8f61802b7d9f2969784f8ad84ef0203a4c170
sunyu1 <sunyu1@green-valley.com>
2019-10-09 16:54:19 +0800

    ad

--- src/view/shoppingMall/index.vue
+++ src/view/shoppingMall/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="main">
       <div class="choose">
         <el-select v-model="value" placeholder="请选择" style="margin-left:20px">
           <el-option
@@ -12,8 +12,8 @@
        <el-input v-model="productName" placeholder="请输入内容" style="width:200px;margin-left:20px"></el-input>
        <el-button style="margin-left:20px">搜索</el-button>
       </div>
-      <el-tabs v-model="activeName" @tab-click="handleClick" style="width:100%">
-        <el-tab-pane label="出售中" name="first">
+      <el-tabs v-model="activeName" @tab-click="handleClick" style="width:100%" >
+        <el-tab-pane label="出售中" name="first" class='sale'>
               <el-table
                 :data="tableData"
                 style="width: 100%">
@@ -21,33 +21,49 @@
                   label="商品"
                   width="180">
                   <template slot-scope="scope">
-                    <span style="">{{ scope.row.date }}</span>
+                   <div class="shopping">
+                     <img :src ="scope.row.image" style="width:70px;height:70px">
+                     <div class="shoppingInfo">
+                       <div>{{ scope.row.productName }}</div>
+                       <div>¥{{ scope.row.price }}</div>
+                     </div>
+                   </div>
                   </template>
                 </el-table-column>
                 <el-table-column
                   label="分组"
                   width="180">
                   <template slot-scope="scope">
-                    <span style="">{{ scope.row.name }}</span>
+                    <span style="">{{ scope.row.productType|productType }}</span>
                   </template>
                 </el-table-column>
                 <el-table-column
                   label="访问统计">
                   <template slot-scope="scope">
-                    <span style="">{{ scope.row.address }}</span>
+                    <div>总流量次数: {{ scope.row.browse?scope.row.browse:0 }}</div>
+                    <div>独立用户: {{ scope.row.browse?scope.row.browse:0 }}</div>
+                    <div>新用户: {{ scope.row.browse?scope.row.browse:0 }}</div>
                   </template>
                 </el-table-column>
-                  <el-table-column
+                <el-table-column
                   prop="address"
                   label="销售统计">
+                  <template slot-scope="scope">
+                    <div>库存:{{ scope.row.stock?scope.row.stock:0 }}</div>
+                    <div>已销售:{{ scope.row.sales?scope.row.sales:0 }}</div>
+                  </template>
                 </el-table-column>
-                   <el-table-column
+                <el-table-column
                   prop="address"
                   label="创建时间">
+                  <template slot-scope="scope">
+                    <div>{{ scope.row.createTime }}</div>
+                  </template>
                 </el-table-column>
-                    <el-table-column
+                <el-table-column
                   prop="address"
                   label="操作">
+                  <div class="operate"><span>编辑</span> | <span>下架</span></div>
                 </el-table-column>
               </el-table>
         </el-tab-pane>
@@ -71,7 +87,7 @@
         value:'',
         pageNumber:0,
         pageSize:10,
-        tableData: []
+        tableData:null
       };
     },
     methods: {
@@ -80,18 +96,24 @@
         console.log(tab, event);
       }
     },
+    filters:{
+      productType:function (value) {
+          return value ===0 ? '处方药':'非处方药'
+      }
+    },
     created() {
       const params = 
       { pageNumber:0,
         pageSize:10}
      this.$http.get('/admin/product',{params}).then((resp)=>{
        console.log('resp_',resp)
-       console.log('resp_',resp)
+        this.tableData = resp.data.pageList
      })
     }
   };
 </script>
 <style lang="scss" scoped>
+.main{
   .choose{
     position:relative;
     left:50%;
@@ -99,4 +121,20 @@
     display:flex;
     justify-content:flex-end;
   }
+  .sale{
+    .shopping{
+      display:flex;
+      .shoppingInfo{
+        margin-left:10px
+      }
+    }
+    .operate{
+      span{
+          cursor:pointer;
+          color:rgb(52,136,255)
+        }
+      }
+  }
+  
+}
 </style>
