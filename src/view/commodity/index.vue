75aecb9caf5a41fb0d8161a4a05b09e8f15f8f77
sunyu1 <sunyu1@green-valley.com>
2019-10-10 10:52:38 +0800

    add

--- src/view/commodity/index.vue
+++ src/view/commodity/index.vue
@@ -1,10 +1,150 @@
 <template>
-  <div>商品管理</div>
+  <div>
+    <el-tabs v-model="activeName">
+    <el-tab-pane label="第一步：填写商品信息" name="first">
+      <el-form ref="form" :model="form" label-width="100px">
+        <el-form-item label="商品名称">
+          <el-col :span="11">
+            <el-input v-model="form.name"></el-input>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="商品封面">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="生产厂家">
+          <el-col :span="11">
+            <el-input v-model="form.name"></el-input>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-col :span="11">
+            <el-input v-model="form.name"></el-input>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="商品分组">
+          <el-col :span="11">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="价格">
+          <el-col :span="11">
+            <el-input v-model="form.name"></el-input>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="删除线价格">
+           <el-col :span="11">
+            <el-input v-model="form.name"></el-input>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="库存">
+           <el-col :span="11">
+            <el-input v-model="form.name"></el-input>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="上架时间">
+          <el-col :span="11">
+               <el-radio-group v-model="form.resource">
+                  <el-radio label="线上品牌商赞助"></el-radio>
+                  <el-radio label="线下场地免费"></el-radio>
+                </el-radio-group>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="商品详情">
+          <el-col :span="11">
+            <el-input type="textarea" v-model="form.resource"></el-input>
+          </el-col>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit" style="width:100px">下一步</el-button>
+          <el-button style="width:100px">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-tab-pane>
+    <el-tab-pane label="第二步：运营选项编辑" name="second"></el-tab-pane>
+  </el-tabs>
+  </div>
 </template>
 <script>
-export default {
+  export default {
+    data() {
+      return {
+        activeName: 'first',
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        imageUrl: ''
+ 
+      };
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      },
+      // 上传图片
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
 
-}
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
+      }
+    },
+    created() {
+    },
+  };
 </script>
-<style lang="scss" scoped>
+
+<style>
+  .avatar-uploader  .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 100px;
+    height: 100px;
+    line-height: 100px;
+    text-align: center;
+  }
+  .avatar-uploader {
+    height: 100px;
+  }
+  .avatar {
+    width: 100px;
+    height: 100px;
+    display: block;
+  }
 </style>
